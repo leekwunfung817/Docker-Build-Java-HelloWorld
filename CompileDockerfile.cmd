3a57c30b551580835e81c7a511c3d1e5e6dd0502
Ivan Lee <leekwunfung817@gmail.com>
2021-09-25 23:41:49 +0800

    l

--- CompileDockerfile.cmd
+++ CompileDockerfile.cmd
@@ -1,6 +1,7 @@
 # Please use the Git bash to run all these things
-REPO_NAME=ivan/helloworld
+REPO_NAME=ivan-helloworld
 
 docker build . -t $REPO_NAME:1.0.0
 docker images | grep $REPO_NAME | awk '{print "docker run "$3}' | bash
+docker save -o $REPO_NAME".tar" $REPO_NAME
 
---
+++ ImportDockerfile.cmd
@@ -0,0 +1,4 @@
+# Please use the Git bash to run all these things
+REPO_NAME=ivan-helloworld
+
+docker load -i $REPO_NAME".tar"
